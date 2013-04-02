3b0809685faa9672d32da5804033b59500fde503
Leif Eriksson <leerikss@gmail.com>
2013-04-02 22:34:28 +0300

    Added a separate gps poller class

--- python/sailersensor.py
+++ python/sailersensor.py
@@ -12,13 +12,13 @@ import socket
 import logging
 import logging.handlers
 import time
-from copy import deepcopy
+from gpspoller import GpsPoller
 
 # Logging
 LOG_FILE='/home/pi/.sailersensor/sailersensor.log'
 logger = logging.getLogger('SailerSensor.kindle')
-# logger.setLevel(logging.DEBUG) # Debug
-logger.setLevel(logging.ERROR) # Production
+logger.setLevel(logging.DEBUG) # Debug
+# logger.setLevel(logging.ERROR) # Production
 handler = logging.handlers.RotatingFileHandler(
           LOG_FILE, maxBytes=1048576, backupCount=3)
 formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s]: %(message)s')
@@ -37,24 +37,27 @@ logger.addHandler(handler)
 # Loop sleep in secs
 LOOP_SLEEP=1
 
-# GPS
-GPS_MIN_TRIES=5
-GPS_MAX_TRIES=10
-
 # Socket
 SOCK_HOST = "192.168.2.2"
 SOCK_TIMEOUT = 5
 SOCK_PORT =    9000
 
+# Nr conversions
+MS_IN_KNOTS = 1.9438444924406
+
 def main():
 
 #    con = None
 
-    prev_rec = None
+    gpsp = GpsPoller()
 
-    while (True):
+    try:
+        gpsp.start()
+        prev_rec = None
 
-        try:
+        while (True):
+
+            try:
             # logger.debug('Connecting to db "%s"...' % DB)
             # con = get_con()
             # init_db(con)
@@ -63,34 +66,39 @@ def main():
             # prv = get_prev_msg(con,GPS_COL_ID)
             # logger.debug('Previous record: %s' % prv)
 
-            logger.debug('=========================')    
-            logger.debug('main() while(True) starts')              
-            logger.debug('Getting GPS sensor data...')  
-            next_rec = get_gps_data()
-            logger.debug('GPS sensor data: "%s"' % next_rec)  
+                logger.debug('=========================')    
+                logger.debug('main() while(True) starts')              
+                logger.debug('Getting GPS sensor data...')  
+                next_rec = get_gps_data( gpsp.get_current_value() )
+                logger.debug('GPS sensor data: "%s"' % next_rec)  
 
-            logger.debug('Building message...')  
-            msg = get_msg_str(prev_rec,next_rec)
-            logger.debug('Message: "%s"' % msg)  
+                logger.debug('Building message...')  
+                msg = get_msg_str(prev_rec,next_rec)
+                logger.debug('Message: "%s"' % msg)  
 
-            logger.debug('Sending message to Kindle (%s:%s)...' % (SOCK_HOST,SOCK_PORT) )  
-            send_to_kindle(msg)
+                logger.debug('Sending message to Kindle (%s:%s)...' % (SOCK_HOST,SOCK_PORT) )  
+                send_to_kindle(msg)
+                
+                logger.debug('Copying data for next round...')
+                prev_rec = copyRec(next_rec)
 
-            prev_rec = deepcopy(next_rec)
-
-            # logger.debug('Storing GPS data into DB...')  
-            # store_msg(con, GPS_COL_ID, nxt)
-            logger.debug('main() while(True) ends. All good.')  
+                # logger.debug('Storing GPS data into DB...')  
+                # store_msg(con, GPS_COL_ID, nxt)
+                logger.debug('main() while(True) ends. All good.')  
             
-        except Exception, e:
-            logger.exception(e)
+            except Exception, e:
+                logger.exception(e)
     
 #        finally:
 #            if con:
 #                con.close()        
 
         # Loop
-        time.sleep(LOOP_SLEEP)
+            time.sleep(LOOP_SLEEP)
+    except (KeyboardInterrupt, SystemExit):
+        logger.error("Sailersensor interrupted. Killing thread(s) and quitting.")
+        gpsp.stop()
+        
 
 def get_con():
     con = sqlite3.connect(DB)
@@ -113,61 +121,35 @@ def get_prev_msg(con,id):
     else:
         return None
 
-def get_gps_data():
+def get_gps_data(packet):
 
-    i=1;
-    session = gps.gps(mode=gps.WATCH_ENABLE)    
+    if( packet is None ):
+        return {}
 
-    while True:
-
-        packet = session.next()
-
-        if packet['class'] == 'TPV':
-
-            session.close()
-
-            ret = {
-                "speed":     packet.get('speed', '?'),
-                "heading":     packet.get('track', '?'),
-                "heeling": 0 # TODO: Maybe add this somewhere else
-            }
-
-            return ret
-
-        else:
-            # TODO: Make this better
-            i+=1
-
-            if( i > GPS_MIN_TRIES):
-                    time.sleep(1)
-                    logger.debug("get_gps_data(): Try nr %s. Running session.next()..." % (i-GPS_MIN_TRIES) )
-
-
-            if(i > GPS_MAX_TRIES):
-                session.close()
-                raise Exception("Unable to retrieve data from GPS sensor. Quitting loop.")
-            
+    return {
+        "speed":     packet.get('speed', '?'),
+        "heading":     packet.get('track', '?')
+        }
 
 def get_msg_str(prv,nxt):
-    msg = build_msg( 'SP', prv, nxt, 'speed', 2, 1 ) + \
+    msg = build_msg( 'SP', prv, nxt, 'speed', MS_IN_KNOTS, 1 ) + \
         build_msg( 'HD', prv, nxt, 'heading' ) + \
         build_msg( 'HL', prv, nxt, 'heeling' )
     return msg
 
 def build_msg(id,prv,nxt,attr,mult=1,dig=0):
-    try:
-        if (prv is None) or ( prv[attr] <> nxt[attr] ):
-            val = nxt[attr]
-            if(mult <> 1):
-                val = val * mult
-            if(dig > 0):
-                val = round(val,dig)
-            else:
-                val = int(val)
-            return '%s%s ' % ( id, val)
+    if (nxt is None) or ( nxt.get(attr,'?') == '?' ):
+        return ''
+    elif ( (prv is None) or prv.get(attr,'?') == '?' or prv[attr] <> nxt[attr] ):
+        val = nxt[attr]
+        if(mult <> 1):
+            val = val * mult
+        if(dig > 0):
+            val = round(val,dig)
         else:
-            return ''
-    except:
+            val = int(val)
+        return '%s%s ' % ( id, val)
+    else:
         return ''
 
 def send_to_kindle(msg):
@@ -176,17 +158,30 @@ def send_to_kindle(msg):
         logger.debug('Message is empty. Not sending anything to Kindle')
         return
 
-    sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    sock.settimeout(SOCK_TIMEOUT)
-    sock.connect((SOCK_HOST,SOCK_PORT))
-    sock.settimeout(None)
-    sock.send(msg)
+    try:
+        sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        sock.settimeout(SOCK_TIMEOUT)
+        sock.connect((SOCK_HOST,SOCK_PORT))
+        sock.settimeout(None)
+        sock.send(msg)
+    except Exception, e:
+        logger.exception(e)
 
 def store_msg(con,id,data):
     cur = con.cursor()
     sql = "REPLACE INTO kindle(id,speed,heading,heeling) VALUES (?,?,?,?)"
     cur.execute(sql,(id,data['speed'],data['heading'],data['heeling']))
     con.commit()
+
+def copyRec(oldRec):
+    rec = {'speed':'?','heading':'?','heeling':'?'}
+    if( oldRec.get('speed','?') != '?' ):
+        rec['speed'] = oldRec['speed']
+    if( oldRec.get('heading','?') != '?' ):
+        rec['heading'] = oldRec['heading']
+    if( oldRec.get('heeling','?') != '?' ):
+        rec['heeling'] = oldRec['heeling']
+    return rec
 
 if __name__ == '__main__': 
     main() 
