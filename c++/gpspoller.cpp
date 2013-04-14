5c5cdbafa2cd0383d312317ce3fa5cdd345d8db9
Leif Eriksson <leerikss@gmail.com>
2013-04-14 19:47:13 +0300

    gpspoller testing

--- c++/gpspoller.cpp
+++ c++/gpspoller.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <unistd.h>
 #include <gps.h>
+#include <libgpsmm.h>
 
 using namespace libconfig;
 using namespace std;
@@ -15,45 +16,72 @@ gpspoller::gpspoller(const Config& cfg)
 
 void gpspoller::run(void)
 {
-  // Init gpsd client
-  gps_open("localhost", DEFAULT_GPSD_PORT);
+  gpsmm gps_rec("localhost", DEFAULT_GPSD_PORT);
 
-  //register for updates
-  gps_stream(gpsdata, WATCH_ENABLE | WATCH_JSON, NULL);
+  if (gps_rec.stream(WATCH_ENABLE|WATCH_JSON) == NULL) {
+    cerr << "No GPSD running.\n";
+    return 1;
+  }
 
-  running = true;
+  for (;;) {
+    struct gps_data_t* newdata;
 
-  while(running)
+    if (!gps_rec.waiting(50000000))
+      continue;
+
+    if ((newdata = gps_rec.read()) == NULL) {
+      cerr << "Read error.\n";
+      return 1;
+    } else {
+      PROCESS(newdata);
+    }
+  }
+
+
+/*
+// Init gpsd client
+gps_open("localhost", DEFAULT_GPSD_PORT, gpsdata);
+
+//register for updates
+gps_stream(gpsdata, WATCH_ENABLE | WATCH_JSON, NULL);
+
+running = true;
+
+while(running)
+{
+//block for up to .5 seconds
+if (gps_waiting(gpsdata, 500))
+{
+//dunno when this would happen but its an error
+if(gps_read(gpsdata)==-1)
+{
+*/
+/*
+  fprintf(stderr,"GPSd Error\n");
+  gps_stream(gpsdata, WATCH_DISABLE, NULL);
+  gps_close(gpsdata);
+  return(-1);
+  break;
+*/
+/*
+  }
+  else
   {
-    //block for up to .5 seconds
-    if (gps_waiting(gpsdata))
-    {
-      //dunno when this would happen but its an error
-      if(gps_read(gpsdata)==-1)
-      {
-      /*
-	fprintf(stderr,"GPSd Error\n");
-	gps_stream(gpsdata, WATCH_DISABLE, NULL);
-	gps_close(gpsdata);
-	return(-1);
-	break;
-      */
-      }
-      else
-      {
-	//status>0 means you have data
-	if(gpsdata->status>0)
+  //status>0 means you have data
+  if(gpsdata->status>0)
 	{
 	  //sometimes if your GPS doesnt have a fix, it sends you data anyways
 	  //the values for the fix are NaN. this is a clever way to check for NaN.
 	  if(gpsdata->fix.longitude!=gpsdata->fix.longitude || gpsdata->fix.altitude!=gpsdata->fix.altitude)
 	  {
+*/
 	  /*
 	    fprintf(stderr,"Could not get a GPS fix.\n");
 	    gps_stream(gpsdata, WATCH_DISABLE, NULL);
 	    gps_close(gpsdata);
 	    return(-1);
 	  */
+/*
 	  }
 	  //otherwise you have a legitimate fix!
 	  else
@@ -68,6 +96,7 @@ void gpspoller::run(void)
     //we want to re-register for updates and keep looping! we dont have a fix yet.
     else
       gps_stream(gpsdata, WATCH_ENABLE | WATCH_JSON, NULL);
+*/
     
     // Sleep
     usleep(s_time);
