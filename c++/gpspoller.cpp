581e2760823132fa82e3214f20e190dcd8927d8c
Leif Eriksson <leerikss@gmail.com>
2013-04-16 00:04:02 +0300

    Removed sleeping from run() if valid data is retrieved. Otherwise a lagging would occur, and newest data would not be retrieved.

--- c++/gpspoller.cpp
+++ c++/gpspoller.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <unistd.h>
 #include <gps.h>
-// #include <libgpsmm.h>
 
 using namespace libconfig;
 using namespace std;
@@ -12,6 +11,41 @@ gpspoller::gpspoller(const Config& cfg)
 {
   // Set vals from config
   s_time = cfg.lookup("gpspoller.sleep");
+}
+
+void gpspoller::run(void)
+{
+  // gpsdata buffer
+  gps_data_t gpsdata_t;
+  gps_data_t* gpsdata = &gpsdata_t;
+
+  // Connect to device
+  open(gpsdata);
+
+  running = true;
+  while(running)
+  {
+    // Read without sleeping as fast as the device spits out data.
+    // First wait max 0.5 secs for data from daemon. 
+    // If is valid (lat nor lon is NaNa), proceed
+    if ( gps_waiting(gpsdata, (500) ) & 
+	 ( gps_read(gpsdata) != -1 ) & 
+	 ( gpsdata->status > 0 ) & 
+	 !(gpsdata->fix.latitude  != gpsdata->fix.latitude) &
+	 !(gpsdata->fix.longitude != gpsdata->fix.longitude) )
+    {
+      // TODO: Do stuff
+      printf("Latitude = %f, longitude = %f\n", gpsdata->fix.latitude, gpsdata->fix.longitude);
+    }
+    // If no valid data is received, sleep to spare CPU
+    else
+    {
+      usleep(s_time);
+    }
+  }
+
+  // Close
+  close(gpsdata);
 }
 
 void gpspoller::open(gps_data_t* gpsdata)
@@ -28,39 +62,4 @@ void gpspoller::close(gps_data_t* gpsdata)
 {
   gps_stream(gpsdata, WATCH_DISABLE, NULL);
   gps_close(gpsdata);
-}
-
-void gpspoller::run(void)
-{
-// gpsdata buffer
-  gps_data_t gpsdata_t;
-  gps_data_t* gpsdata = &gpsdata_t;
-
-// Connect to device
-  open(gpsdata);
-
-  running = true;
-  while(running)
-  {
-    //block to wair for data
-    if (gps_waiting(gpsdata, (500)))
-    {
-      // Data ok if it is read, has a status, and latitude nor 
-      // longitude is NaN
-      if( ( gps_read(gpsdata) != -1 ) & ( gpsdata->status > 0 ) & 
-	  !(gpsdata->fix.latitude  != gpsdata->fix.latitude) &
-	  !(gpsdata->fix.longitude != gpsdata->fix.longitude) )
-      {
-	// TODO: Do stuff
-	printf("Latitude = %f, longitude = %f\n", gpsdata->fix.latitude, gpsdata->fix.longitude);
-      }
-      else
-	cerr << "No data retrieved." << endl;
-    }
-    // Sleep
-    usleep(s_time);
-  }
-
-  // Close
-  close(gpsdata);
 }
