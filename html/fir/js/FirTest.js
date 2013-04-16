6cc5d6cbe29ed05f43005cfbdb69767d7aacc0bb
Leif Eriksson <leerikss@gmail.com>
2013-04-16 21:49:02 +0300

    Re-indentation

--- html/fir/js/FirTest.js
+++ html/fir/js/FirTest.js
@@ -5,22 +5,26 @@ function FirTest()
 {
     
   // Init map 
-  var options = {
-  mapTypeId: google.maps.MapTypeId.ROADMAP,
-  center: new google.maps.LatLng(60.169845,24.938551), // <-- center to HELSINKI perkele!
-  scaleControl: true,
-  zoom: 18
-  };
+  var options = 
+    {
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    center: new google.maps.LatLng(60.169845,24.938551), // <-- center to HELSINKI perkele!
+    scaleControl: true,
+    zoom: 18
+    };
+
   this.map = new google.maps.Map( $("#map")[0], options );
   this.rawLine = new google.maps.Polyline([]);
   this.firLine = new google.maps.Polyline([]);
 	
   // Map listener
   var _this = this;
-  google.maps.event.addListener(this.map, "click", 
-				function(event) { _this.mapClicked(event); _this.drawRawPath(); } );
+  google.maps.event.addListener(
+    this.map, "click", 
+    function(event) { _this.updateJson(event); _this.drawRawPath(); } 
+    );
 		
-  // Init textarea event
+  // Select all when textarea clicked
   $("textarea").focus(function() {
       var $this = $(this);
       $this.select();	
@@ -85,9 +89,10 @@ FirTest.prototype.drawFirPath = function()
     this.loCount = 0;
     var lat = parseFloat( json[0].latitude );
     var lng = parseFloat( json[0].longitude );
-    // Init buffer with first lat/lng value
-    var rawLats = this.getArray( coefs.length, lat );
-    var rawLngs = this.getArray( coefs.length, lng );
+
+    // Init buffers with first lat/lng value
+    var oldLats = this.getArray( coefs.length, lat );
+    var oldLngs = this.getArray( coefs.length, lng );
 		
     // Clear all previous paths
     this.clearMap(false,true);
@@ -101,8 +106,8 @@ FirTest.prototype.drawFirPath = function()
       // Get FIR filtered lat/lng
       lat = parseFloat(o.latitude);
       lng = parseFloat(o.longitude);
-      lat = this.getFirValue( lat, rawLats, coefs, this.laCount++ );
-      lng = this.getFirValue( lng, rawLngs, coefs, this.loCount++ );
+      lat = this.getFirValue( lat, oldLats, coefs, this.laCount++ );
+      lng = this.getFirValue( lng, oldLngs, coefs, this.loCount++ );
       var ll = new google.maps.LatLng( lat, lng );
       path.push( ll );
     }		
@@ -124,7 +129,7 @@ FirTest.prototype.drawFirPath = function()
   }
 };
 
-FirTest.prototype.mapClicked = function(event)
+FirTest.prototype.updateJson = function(event)
 {
   var v = $("#taCoords").val();
 
@@ -145,20 +150,20 @@ FirTest.prototype.mapClicked = function(event)
   '}';
 		
   $("#taCoords").val( '[' + v + nv + ']'); 
-}
+};
 
-  FirTest.prototype.clearMap = function(clearRaw,clearFir)
+FirTest.prototype.clearMap = function(clearRaw,clearFir)
 {
   if(clearRaw)
     this.rawLine.setPath([]);
   if(clearFir)
     this.firLine.setPath([]);
-}
+};
 
-  /***************
-   * FIR METHODS *
-   **************/
-    FirTest.prototype.parseFirCoefs = function(coefs)
+/***************
+ * FIR METHODS *
+ **************/
+FirTest.prototype.parseFirCoefs = function(coefs)
 {
   var txt = $("#taCoefs").val();
   var arr = txt.split("\n");
@@ -177,19 +182,19 @@ FirTest.prototype.getArray = function(len,val)
   var arr = new Array(len);
   while(--len >= 0) { arr[len] = val; }
   return arr;
-}
+};
 
-FirTest.prototype.getFirValue = function(rawVal, rawArr, coefsArr, count)
+FirTest.prototype.getFirValue = function(val, oldVals, coefs, count)
 {
-  // Store raw data
-  rawArr[count] = rawVal;
+  // Add new val to buffer
+  oldVals[count] = val;
 
-  var len = coefsArr.length;
+  var len = coefs.length;
   var result = 0;
   var index = count;
   for(var i=0; i<len; i++)
   {
-    result += coefsArr[i] * rawArr[index--];
+    result += coefs[i] * oldVals[index--];
 		
     if( index < 0 )
       index = ( len - 1 );
