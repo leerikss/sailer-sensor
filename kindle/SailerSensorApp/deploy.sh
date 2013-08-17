e1b3d229616db86ce2659e730d860021c36ad420
Leif Eriksson <leerikss@gmail.com>
2013-08-17 23:06:53 +0300

    Deleted usbnet folder, added a merged keystore

--- kindle/SailerSensorApp/deploy.sh
+++ kindle/SailerSensorApp/deploy.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
 FILENAME=SailerSensorApp
-KEYSTORE=developer.keystore
+KEYSTORE=sailersensor.keystore
 YOUR_ALIAS=Test
+PASSWORD=password
 JAR=$FILENAME.azw2
 MANIFEST=$FILENAME.manifest
 SSH_USER=root
-SSH_IP=kindle.intra
+SSH_IP=kindle.cable
 
 set -e
 ant jar
 cp $FILENAME.jar $JAR
-jarsigner -keystore $KEYSTORE -storepass password $JAR dk$YOUR_ALIAS
-jarsigner -keystore $KEYSTORE -storepass password $JAR di$YOUR_ALIAS
-jarsigner -keystore $KEYSTORE -storepass password $JAR dn$YOUR_ALIAS
+jarsigner -keystore $KEYSTORE -storepass $PASSWORD $JAR dk$YOUR_ALIAS
+jarsigner -keystore $KEYSTORE -storepass $PASSWORD $JAR di$YOUR_ALIAS
+jarsigner -keystore $KEYSTORE -storepass $PASSWORD $JAR dn$YOUR_ALIAS
 
-scp $JAR $SSH_USER@$SSH_IP:/mnt/us/documents
+# scp $JAR $SSH_USER@$SSH_IP:/mnt/us/documents
