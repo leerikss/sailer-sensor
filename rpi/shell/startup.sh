6d107e55260eecf53d8e824e9865e24b557e6e11
Leif Eriksson <leerikss@gmail.com>
2013-08-22 15:29:52 +0300

    Reinstroduces bringing up wlano/eth0 interfaces, since there were issues with the wlan

--- rpi/shell/startup.sh
+++ rpi/shell/startup.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Start wireless Access Point
+sudo ifconfig wlan0 192.168.5.100 up
+
+# Start udhcpd
+service udhcpd restart
+
+# Start eth0
+sudo ifconfig eth0 192.168.3.100 up 
+
 # Set time from GPS
 /home/pi/sailer-sensor/rpi/shell/set_time.sh &
 
