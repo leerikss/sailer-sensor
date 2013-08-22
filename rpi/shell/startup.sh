8eebed4ea8eedfa17715c5ed6210c253752ddb1a
Leif Eriksson <leerikss@gmail.com>
2013-08-22 14:06:52 +0300

    Changes gps time > date script

--- rpi/shell/startup.sh
+++ rpi/shell/startup.sh
@@ -2,14 +2,13 @@
 
 # Set time from gpsd
 # From: http://blog.petrilopia.net/linux/raspberry-pi-set-time-gps-dongle/
-date -s '01/01/2014 00:01'
+# date -s '01/01/2014 00:01'
 sleep 1
 pkill ntpd
 pkill gpsd
 gpsd -b -n -D 2 /dev/ttyUSB0
 sleep 2
-GPSDATE=`gpspipe -w | head -10 | grep TPV | sed -r 's/.*"time":"([^"]*)".*/\1/' | head -1`
-echo $GPSDATE
+GPSDATE=`gpspipe -w | head -20 | grep TPV | sed -r 's/.*"time":"([^"]*)".*/\1/' | head -1`
 date -s "$GPSDATE"
 /usr/sbin/ntpd
 
